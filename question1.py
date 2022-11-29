c3af42292b0ebad00fcfadcf3656f700da8e2e49
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-29 14:37:56 -0300

    feat: changed function name

--- question1.py
+++ question1.py
@@ -1,5 +1,4 @@
-# LEMBRAR DE CRIAR A CLASSE
-def solve1(list):
+def find_underpaid(list): # -> function to find and print employes with more than 3 years with a salary below the average
     # calculates average
     sum = 0
     for i in range(0,len(list1)):
@@ -12,7 +11,8 @@ def solve1(list):
             print(list1[i][0]) # print employer's name
     
 
-
+# input
 list1 = [["Bruno", 1700.00, 3], ["Leonardo", 1400.23, 1], ["Juan", 1561.12, 2], ["Juliana", 1660.07,3], ["Wagner", 1841.92, 5], ["Micaela", 2000.00, 1], ["Bento", 1750.87, 4], ["Lucia", 1600.55,1], ["Pedro", 1690.00, 4], ["Carla", 1580.00, 6]]
 
-solve1(list1)
+# calling function
+find_underpaid(list1)
