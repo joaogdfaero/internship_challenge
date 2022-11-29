bcb8b8d82698ba271e4980811da7f0a4f50a6799
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-29 14:36:00 -0300

    feat: problem 1 done (without tests)

--- question1.py
+++ question1.py
@@ -1,13 +1,18 @@
 # LEMBRAR DE CRIAR A CLASSE
 def solve1(list):
-    print(list[0][0])
+    # calculates average
+    sum = 0
+    for i in range(0,len(list1)):
+        sum = sum + list1[i][1]
+    average = sum/len(list1)
+    
+    # iterate to print employees with more than 3 years with a salary below the average
+    for i in range(0,len(list1)):
+        if(list1[i][1] < average and list1[i][2]>3 ): #if salary is below average AND employee is with the company for more than 3 years than:
+            print(list1[i][0]) # print employer's name
+    
 
 
-
-
-
-list1 = [["Bruno", 1700.00, 3], ["Leonardo", 1400.23, 1], ["Juan", 1561.12, 2], ["Juliana", 1660.07,
-3], ["Wagner", 1841.92, 5], ["Micaela", 2000.00, 1], ["Bento", 1750.87, 4], ["Lucia", 1600.55,
-1], ["Pedro", 1690.00, 4], ["Carla", 1580.00, 6]]
+list1 = [["Bruno", 1700.00, 3], ["Leonardo", 1400.23, 1], ["Juan", 1561.12, 2], ["Juliana", 1660.07,3], ["Wagner", 1841.92, 5], ["Micaela", 2000.00, 1], ["Bento", 1750.87, 4], ["Lucia", 1600.55,1], ["Pedro", 1690.00, 4], ["Carla", 1580.00, 6]]
 
 solve1(list1)
