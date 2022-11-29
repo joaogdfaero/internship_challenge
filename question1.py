79ae78019c7812f0656f654b47ee221aec4c026e
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-29 14:19:50 -0300

    feat: init first problem

---
+++ question1.py
@@ -0,0 +1,13 @@
+# LEMBRAR DE CRIAR A CLASSE
+def solve1(list):
+    print(list[0][0])
+
+
+
+
+
+list1 = [["Bruno", 1700.00, 3], ["Leonardo", 1400.23, 1], ["Juan", 1561.12, 2], ["Juliana", 1660.07,
+3], ["Wagner", 1841.92, 5], ["Micaela", 2000.00, 1], ["Bento", 1750.87, 4], ["Lucia", 1600.55,
+1], ["Pedro", 1690.00, 4], ["Carla", 1580.00, 6]]
+
+solve1(list1)
